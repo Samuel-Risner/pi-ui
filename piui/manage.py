b30806c6b30c5a41a2cef63a15900ffa88c23dac
Samuel Risner <96385448+Samuel-Risner@users.noreply.github.com>
2023-09-13 07:24:59 +0000

    Configured shebang to use the virtual environment

--- piui/manage.py
+++ piui/manage.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python
+#!./venv/bin/python3.11
+
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
@@ -16,7 +17,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
